bfaef777622c1e0273b846ca70780eb43860e7d0
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-07-28 16:56:09 -0300

    feat - transferindo rotas para o App

--- frontend/blog-tdc/src/App.jsx
+++ frontend/blog-tdc/src/App.jsx
@@ -1,9 +1,19 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
 
   return (
-    <div className="App">
-      Vite APP
+    // <div className="App">
+    //   Vite APP
+    // </div>
+    <Router>
+    <div className="pt-20">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
+    </Router>
   )
 }
 
