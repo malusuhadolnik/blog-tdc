7b7f9fc6551f9a5f4715955df683616934ca61a1
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-07-30 17:08:34 -0300

    arquivo de mock para os posts

---
+++ frontend/blog-tdc/src/components/LoadPosts.jsx
@@ -0,0 +1,50 @@
+import { useContext, useEffect, useState } from 'react';
+import AppContext from '../context/AppContext'; // a ser criado
+import { requestAllData } from '../services/requests'; // a ser criado
+import Card from './Card';
+
+function LoadPosts() {
+  const { dbPosts, setdbPosts } = useContext(AppContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Para usar quando o back-end estiver pronto
+  const postsList = async () => {
+    try {
+      const data = await requestAllData('/posts'); // endpoint para o fetch dos posts no db, a ser criado
+      setdbPosts(data);
+      setIsLoading(false);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading === true) postsList();
+  }, []);
+  
+  return (
+    <div className="container-products">
+      <h2>Últimos posts</h2>
+      {
+        !isLoading ? (
+          <div className="posts-container">
+            {dbPosts.length > 0 && dbPosts.map((post) => (<Card
+              key={ post.id }
+              title={ post.title }
+              mainCategory={ post.category }
+              summary={ post.summary }
+              mainImage={ post.mainImage }
+              id={ post.id }
+            />))}
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )
+      }
+    </div>
+
+  );
+}
+
+export default LoadPosts;
---
+++ frontend/blog-tdc/src/mocks/mockPosts.jsx
@@ -0,0 +1,28 @@
+const Posts = [
+    {
+      id: 1,
+      title: 'Mock Post 1',
+      mainCategory: 'organization',
+      summary: 'Resumo do post 1',
+      text: 'Ser ou não ser, eis a questão: será mais nobre Em nosso espírito sofrer pedras e setas Com que a Fortuna, enfurecida, nos alveja, Ou insurgir-nos contra um mar de provocações E em luta pôr-lhes fim? Morrer… dormir: não mais.',
+      mainImage: 'https://unsplash.com/pt-br/fotografias/KZocXyI3kRo',
+    },
+    {
+        id: 2,
+        title: 'Mock Post 2',
+        mainCategory: 'creativity',
+        summary: 'Resumo do post 2',
+        text:'Dormir… Talvez sonhar: eis onde surge o obstáculo: Pois quando livres do tumulto da existência, No repouso da morte o sonho que tenhamos Devem fazer-nos hesitar: eis a suspeita Que impõe tão longa vida aos nossos infortúnios.',
+        mainImage: 'https://unsplash.com/pt-br/fotografias/l3N9Q27zULw',
+    },
+    {
+        id: 3,
+        title: 'Mock Post 3',
+        mainCategory: 'stationary',
+        summary: 'Resumo do post 3',
+        text:'O pensamento assim nos acovarda, e assim É que se cobre a tez normal da decisão Com o tom pálido e enfermo da melancolia; E desde que nos prendam tais cogitações, Empresas de alto escopo e que bem alto planam Desviam-se de rumo e cessam até mesmo De se chamar ação.',
+        mainImage: 'https://unsplash.com/pt-br/fotografias/l3N9Q27zULw',
+    },
+]
+
+export default Posts
