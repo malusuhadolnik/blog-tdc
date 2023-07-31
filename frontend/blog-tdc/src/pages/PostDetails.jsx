388028939f55b5b195859d25179e650ce25bbc0e
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-07-31 15:07:56 -0300

    feat - componente FullPostDetails

---
+++ frontend/blog-tdc/src/pages/PostDetails.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+function PostDetails() {
+    return (
+        <main>
+          <Header />
+          <Navbar />
+          {/* <div className="body">
+           // renderizar componente do post completo
+          </div> */}
+        </main>
+      );
+}
+
+export default PostDetails;
